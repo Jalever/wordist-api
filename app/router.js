d7946d0ab1deb4e0c91b1cf32ed21f20a334f2ea
Jalever <jaleverchen@gmail.com>
2020-07-14 16:08:18 +0800

    update

--- app/router.js
+++ app/router.js
@@ -15,14 +15,11 @@ module.exports = app => {
   router.post('/api/user/login', controller.users.login);
   router.get('/api/users/all', app.jwt, controller.users.getAllUsers);
 
-<<<<<<< HEAD
-=======
 
   // articles
   router.get('/api/article/all', app.jwt, controller.articles.all);
   router.post('/api/article/create', app.jwt, app.controller.articles.create);
 
->>>>>>> eac2b2c45854a39b6829eebb801f3da07e79700a
   // wordsheet
   router.get('/api/wordsheet/all', app.jwt, controller.wordsheet.all);
   router.post('/api/wordsheet/create', app.jwt, controller.wordsheet.create);
